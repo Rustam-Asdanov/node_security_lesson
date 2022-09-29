c1f3ebd2a5d25238c8bcfc7fcb43b2d0ca520449
Rustam-Asdanov <asdanovrustam@gmail.com>
2022-09-29 18:00:22 +0400

    level 4 is ended

--- controller/authorization_controller.js
+++ controller/authorization_controller.js
@@ -1,4 +1,6 @@
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const salt_round = 12;
+
 const { addUser, findUser } = require("../service/authorization_service");
 
 const loginPage = (req, res) => {
@@ -8,9 +10,15 @@ const loginPage = (req, res) => {
 const loginUser = async (req, res) => {
   await findUser(req.body)
     .then((response) => {
-      if (response.password === md5(req.body.password)) {
-        res.redirect("/main/secrets");
-      }
+      if (response)
+        bcrypt
+          .compare(req.body.password, response.password)
+          .then((result) => {
+            if (result) res.redirect("/main/secrets");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
     })
     .catch((err) => {
       console.log(err);
@@ -23,10 +31,17 @@ const registrationPage = (req, res) => {
 
 const createUser = async (req, res) => {
   const body = req.body;
-  body.password = md5(body.password);
-  await addUser(req.body)
-    .then((response) => {
-      res.redirect("/main/secrets");
+  bcrypt
+    .hash(body.password, salt_round)
+    .then(async (hash) => {
+      body.password = hash;
+      await addUser(req.body)
+        .then((response) => {
+          res.redirect("/main/secrets");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     })
     .catch((err) => {
       console.log(err);
